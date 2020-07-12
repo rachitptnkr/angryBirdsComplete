64120525b0af026248dd7c77df82748be41ca036
Rachit <rachitptnkr@gmail.com>
2020-07-12 11:43:10 +0530

    hi

--- sketch.js
+++ sketch.js
@@ -78,12 +78,14 @@ function draw(){
     platform.display();
     //log6.display();
     slingshot.display();    
+
+
 }
 
 function mouseDragged(){
-    //if (gameState!=="launched"){
+    if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+    }
 }
 
 
@@ -93,8 +95,11 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
+    if(keyCode === 32 && bird.body.speed<1){
+        bird.trajectory=[]
+        Matter.Body.setPosition(bird.body,{x:200,y:50})
        slingshot.attach(bird.body);
+       gameState="onSling"
     }
 }
 
@@ -104,8 +109,9 @@ async function getBackgroundImg(){
 
     var datetime = responseJSON.datetime;
     var hour = datetime.slice(11,13);
+    console.log(hour)
     
-    if(hour>=0600 && hour<=1900){
+    if(hour>=06 && hour<=19){
         bg = "sprites/bg1.png";
     }
     else{
